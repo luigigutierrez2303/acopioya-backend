f989bb51ce379adb55344535445b9a5b0f0867ac
Luigi Gutierrez <luigigutierrez2303@gmail.com>
2026-06-26 23:15:39 -0300

    Aplicar migraciones automaticamente al arrancar en produccion

--- AcopioYA.Api/Program.cs
+++ AcopioYA.Api/Program.cs
@@ -85,6 +85,9 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+
     await DbSeeder.SeedAsync(scope.ServiceProvider, app.Configuration);
 }
 
